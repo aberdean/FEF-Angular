1b1f2475705353cc9be2d122dcf52b6ad6fc350c
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2018-05-17 10:08:01 +0200

    Add UI-Router

--- UdaciMeals/app/scripts/app.js
+++ UdaciMeals/app/scripts/app.js
@@ -9,4 +9,26 @@
  * Main module of the application.
  */
 angular
-  .module('udaciMealsApp', []);
+  .module('udaciMealsApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  	$urlRouterProvider.otherwise('/');
+  	$stateProvider
+	  	.state('home', {
+	  		url: '/',
+	  		templateUrl: 'views/menu.html',
+	  		controller: 'MenuCtrl as menu'
+	  	})
+	  	.state('item', {
+			url: '/item/:id',
+			templateUrl: 'views/item.html',
+			controller: 'ItemCtrl as item'
+		})
+		.state('item.nutrition', {
+			url: '/nutrition',
+			templateUrl: 'views/item-nutrition.html'
+		})
+		.state('item.reviews', {
+			url: '/reviews',
+			templateUrl: 'views/item-reviews.html'
+		});
+  }]);
